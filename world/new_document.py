a7bcfedc9d3c0cf739022eb3219f93c2d59ee340
Daniel Cronin <daniel@dcron.in>
2026-02-15 17:43:42 -0600

    fix: Add missing content assignment in new_document.ev to ensure proper document initialization.

---
+++ world/new_document.py
@@ -0,0 +1,27 @@
+new_doc = evennia.create_object("typeclasses.documents.Document")
+
+new_doc.db.content = """
+# Discussion Assignment: Shortcuts
+Pick an application you use a lot (examples: Google Docs, Google Sheets, Microsoft Word, Adobe Premiere, VS Code, etc.). Your goal is to find and test a keyboard shortcut that helps you work faster.
+
+## Steps
+- Choose an application you use frequently.
+- Find a list of keyboard shortcuts for that application.
+    - Examples of what a shortcut looks like: Ctrl + C (copy), Ctrl + V (paste), Ctrl + Z (undo).
+- From that list, choose one shortcut that:
+    - You have never used before
+    - You think would be useful
+    - No one else has already posted for that same application
+- Give it a test run (actually try it in the application).
+## Your post
+- Application Name:
+    - (What program did you try the shortcut in?)
+- Keyboard Shortcut Discovered:
+    - (Exactly which keys do you press? Example: Ctrl + Shift + N)
+- What This Shortcut Does:
+    - (Briefly explain the function or action this shortcut performs.)
+- Will you keep using it? Why or why not?
+    - (After testing it out, do you think this shortcut will become part of your routine? Explain your reasoning.)
+## Replies / reactions
+Replies are semi-optional, but please put at least 2 thumbs up (or similar) under shortcuts you think are worthwhile so classmates can quickly find the ones that matter.
+"""
