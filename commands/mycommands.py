f165749b039f3484b49db19339a77c4c8ed58069
Daniel Cronin <daniel@dcron.in>
2026-02-15 12:20:27 -0600

    feat: enhance the 'hit' command to parse and include an optional weapon in the action message.

--- commands/mycommands.py
+++ commands/mycommands.py
@@ -23,17 +23,35 @@ class CmdHit(Command):
 
     key = 'hit'
 
+    def parse(self):
+        self.args = self.args.strip()
+        target, *weapon = self.args.split(" with ", 1)
+        if not weapon:
+            target, *weapon = target.split(" ", 1)
+        self.target = target.strip()
+        if weapon:
+            self.weapon = weapon.strip()
+        else:
+            self.weapon = ""
+
     def func(self):
-        args = self.args.strip()
-        if not args:
+        if not self.target:
             self.caller.msg("Hit what?")
             return
         
-        target = self.caller.search(args)
+        target = self.caller.search(self.target)
         if not target:
             return
-        self.caller.msg(f"You hit {target} in the face!")
-        target.msg(f"{self.caller} hits you in the face!")
+
+        if self.weapon:
+            weapon = self.caller.search(self.weapon)
+            if weapon:
+                weaponstr = f"{weapon.key}"
+            else:
+                weaponstr = "bare fists"
+
+        self.caller.msg(f"You hit {target} in the face with their {weaponstr}!")
+        target.msg(f"{self.caller} hits you in the face with their {weaponstr}!")
 
 
 class MyCmdSet(CmdSet):
