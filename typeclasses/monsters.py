78b0e526b6755eed9ca6c2b1c4ee7941362ac42b
Daniel Cronin <daniel@dcron.in>
2026-02-14 20:04:56 -0600

    test

--- typeclasses/monsters.py
+++ typeclasses/monsters.py
@@ -1,11 +1,11 @@
 from evennia.objects.objects import DefaultObject
 
-class Monster(DefaultObject):
+class Monster(DefaultObject, name):
     key = "Monster"
 
     def at_init(self, name):
         print(f"{self.key} is initialized")
-        self.key = "name"
+        self.key = name
 
     def move_around(self):
         print(f"{self.key} moves around")
