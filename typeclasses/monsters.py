739774712205eb0dd2b997fc56f701010ab3542a
Daniel Cronin <daniel@dcron.in>
2026-02-14 19:59:26 -0600

    testing init

--- typeclasses/monsters.py
+++ typeclasses/monsters.py
@@ -1,5 +1,9 @@
 class Monster:
     key = "Monster"
 
+    def at_init(self, name):
+        print(f"{self.key} is initialized")
+        self.key = "name"
+
     def move_around(self):
         print(f"{self.key} moves around")
