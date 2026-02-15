7d7302373103824715895f042b2d6054a77d379e
Daniel Cronin <daniel@dcron.in>
2026-02-14 21:20:28 -0600

    update mycmdset

--- commands/mycommands.py
+++ commands/mycommands.py
@@ -4,12 +4,9 @@ from evennia import CmdSet
 
 class CmdEcho(Command):
     key = 'echo'
-    help_category = 'Fun'
 
-    
+
 class MyCmdSet(CmdSet):
-    key = 'my_cmd_set'
-    priority = 10
 
     def at_cmdset_creation(self):
         self.add(CmdEcho())
